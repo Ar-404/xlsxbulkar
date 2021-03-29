c8b414af3853b2cc72694825dfd55e90fb60a5bd
Ar-404 <wolfweb19@gmail.com>
2021-03-29 22:27:54 +0530

    index error solve

--- index.js
+++ index.js
@@ -1,39 +1,32 @@
-import * as XLSX from 'xlsx';
+var  XLSX = require('xlsx');
+var workBook = null;
+var jsonData = null;
+var reader = new FileReader();
+var file = null;
+var data =null;
 
 function xlsxbulkar(ev){
 
-  let workBook = null;
-  this.jsonData = null;
-  const reader = new FileReader();
-  const file = ev.target.files[0];
+  console.log("get :",ev);
+  file = ev.target.files[0];
+  console.log("file :",file);
   reader.onload = async (event) => {
-    const data = reader.result;
+    data = reader.result;
+    // console.log(data);
     workBook = XLSX.read(data, { type: 'binary' });
-    this.jsonData = workBook.SheetNames.reduce((initial, name) => {
-      const sheet = workBook.Sheets[name];
+    jsonData = workBook.SheetNames.reduce((initial, name) => {
+      let sheet = workBook.Sheets[name];
+      console.log("sheet :",sheet);
       initial[name] = XLSX.utils.sheet_to_json(sheet);
       return initial;
     }, {});
-    this.dataString = JSON.stringify(this.jsonData);
-     this.appServiceProvider.console_log("Data:",this.jsonData.data);
-    //  console.log("Data:",this.jsonData.data);
-    //  console.log("DataArray:",this.dataArray);
-     await this.jsonData.data.forEach((data,i)=>{ 
-      // console.log("data",data);
-       data.gstfullplate = Number(data.fullplate * this.gstValue) / 100;
-       let name = data.itemname.toLowerCase();
-       data.itemname = name;
-       data['seq']=i;
-     });
+    let dataString = JSON.stringify(jsonData);
+     console.log("Data:",jsonData.Sheet1);
+     console.log("DataArray:",dataString);
 
-      console.log("Data to upload:",this.jsonData.data);
-    //   this.jsonData.data.forEach(element => {
-    //     console.log(element);
-    //     this.pouchProvider.menulistBulk(element).then((doc=>{
-    //     console.log(doc);
-        
-    //   })).catch(e=>console.log(e));
-    //   });
+      jsonData.data.forEach(element => {
+        console.log(element);
+      });
     }
   reader.readAsBinaryString(file);
 }
