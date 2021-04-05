53d92ab720baf673644e5402a809be9c31fa6031
Ar-404 <wolfweb19@gmail.com>
2021-04-05 19:57:18 +0530

    modified

--- index.js
+++ index.js
@@ -6,29 +6,21 @@ var file = null;
 var data =null;
 
 function xlsxbulkar(ev){
-
-  console.log("get :",ev);
+  return new Promise((resolve, reject)=>{
   file = ev.target.files[0];
   console.log("file :",file);
   reader.onload = async (event) => {
     data = reader.result;
-    // console.log(data);
     workBook = XLSX.read(data, { type: 'binary' });
     jsonData = workBook.SheetNames.reduce((initial, name) => {
       let sheet = workBook.Sheets[name];
-      console.log("sheet :",sheet);
       initial[name] = XLSX.utils.sheet_to_json(sheet);
       return initial;
     }, {});
-    let dataString = JSON.stringify(jsonData);
-     console.log("Data:",jsonData.Sheet1);
-     console.log("DataArray:",dataString);
-
-      jsonData.data.forEach(element => {
-        console.log(element);
-      });
+    resolve(jsonData.data);
     }
   reader.readAsBinaryString(file);
+  })
 }
 
 module.exports.xlsxbulkar = xlsxbulkar;
